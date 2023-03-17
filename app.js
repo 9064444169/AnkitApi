46e759b55e99f5427979d120d98228519839117d
ankit <ankitxtri@gmail.com>
2023-03-17 09:34:20 +0530

    host api live

---
+++ app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const connectDB = require("./db/connect");
+
+const PORT = process.env.PORT || 5001;
+
+const products_routes = require("./routes/products");
+
+app.get("/", (req, res) => {
+  res.send("Hi,I am live");
+});
+
+// middleware or to set router
+app.use("/api/products", products_routes);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MongoDB_URL);
+    app.listen(PORT, () => {
+      console.log(`connected on port: ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ controllers/products.js
@@ -0,0 +1,56 @@
+const Product = require("../models/product");
+
+const getAllProducts = async (req, res) => {
+  const { company, name, featured, sort, select } = req.query;
+  const queryObject = {};
+
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (featured) {
+    queryObject.featured = featured;
+  }
+
+  if (name) {
+    // queryObject.name = name;
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  let apiData = Product.find(queryObject);
+
+  if (sort) {
+    // let sortFix = sort.replace(",", " ");
+    // queryObject.sort = sortFix;
+    let sortFix = sort.split(",").join(" ");
+    apiData = apiData.sort(sortFix);
+  }
+
+  //select will exlude the other things
+  if (select) {
+    // let selectFix = select.replace(",", " ");
+    let selectFix = select.split(",").join(" ");
+    apiData = apiData.select(selectFix);
+  }
+
+  //pagination page and limit
+  let page = Number(req.query.page) || 1;
+  let limit = Number(req.query.limit) || 10;
+
+  let skip = (page - 1) * limit; //formula for pagination
+
+  apiData = apiData.skip(skip).limit(limit);
+
+  // console.log(queryObject);
+
+  const Products = await apiData;
+  res.status(200).json({ Products, nbHits: Products.length });
+};
+
+const getAllProductsTesting = async (req, res) => {
+  const myData = await Product.find(req.query).skip(1);
+
+  res.status(200).json({ myData });
+};
+
+module.exports = { getAllProducts, getAllProductsTesting };
